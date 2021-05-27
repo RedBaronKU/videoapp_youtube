007d792adf9b8ea37f05b18b83a710c538b85b5d
RedBaronKU <kr.utkarsh106@gmail.com>
2021-05-28 03:50:21 +0530

    homepage_complete

--- youtube/src/components/Videolist.js
+++ youtube/src/components/Videolist.js
@@ -1,59 +1,66 @@
-import React from 'react'
 export const Videolist=[
     {
-        thumbnail: '../images/jonathan.jpg',
+        thumbnail: './images/jonathan.jpg',
         uploader_pic:'https://yt3.ggpht.com/ytc/AAUvwnhJk7lLwniUqMFWeskdmTuIBKuSESw27pkwR_KZIg=s48-c-k-c0x00ffffff-no-rj-mo',
         title: 'TIPS THAT CAN HELP YOU WIN CLOSE COMBAT | HOW TO SET YOUR CONTROLS AND SENSITIVITY | PUBG MOBILE',
         uploader_name: 'Jonathan Gaming',
-        upload_time:'2 years ago'
+        upload_time:'2 years ago',
+        views: '87M views'
     },
     {
-        thumbnail: '../images/mavi.jpg',
+        thumbnail: './images/mavi.jpg',
         uploader_pic:'https://yt3.ggpht.com/ytc/AAUvwnh1osn8-ckm-CDSTICrcBX4Hu-1SB4YDi_4p22C7w=s48-c-k-c0x00ffffff-no-rj-mo',
         title: 'GOODNIGHT STREAM BGMI CHILL | Battlegrounds Mobile India GRIND',
         uploader_name: 'Toxic Mavi',
-        upload_time:'1 year ago'
+        upload_time:'1 year ago',
+        views: '8M views'
     },
     {
-        thumbnail: '../images/snax.jpg',
+        thumbnail: './images/snax.jpg',
         uploader_pic:'https://yt3.ggpht.com/ytc/AAUvwngJjycxlL5C8p1OrAAt99tmOegfiNddqT-8y6sGUA=s68-c-k-c0x00ffffff-no-rj-mo',
         title: 'M416 + DP28 COMBO IN T1 SCRIMS WWCD - PUBG MOBILE',
         uploader_name: 'Snax Gaming',
-        upload_time:'3 years ago'
+        upload_time:'3 years ago',
+        views: '45K views'
     },
     {
-        thumbnail: '../images/regaltos.jpg',
+        thumbnail: './images/regaltos.jpg',
         uploader_pic:'https://yt3.ggpht.com/ytc/AAUvwnhhg3vyA5kPB4xF1yQSeTvD7D5K6x52I4v4Mz8Ldw=s176-c-k-c0x00ffffff-no-rj-mo',
         title: 'ROAD TO 800K | CRATE OPENING TODAY? | Regaltos',
         uploader_name: 'Regaltos Gaming',
-        upload_time:'3 days ago'
+        upload_time:'3 days ago',
+        views: '858K views'
     },
     {
-        thumbnail: '../images/gun.jpg',
+        thumbnail: './images/gun.jpg',
         uploader_pic:'https://yt3.ggpht.com/ytc/AAUvwnjln0WuTMFiDQZyoTlHiHOecDPqSEBWoF8PMXyjgw=s48-c-k-c0x00ffffff-no-rj-mo',
         title: 'Get Ready To Fight | BAAGHI',
         uploader_name: 'Ajju Bro',
-        upload_time:''
+        upload_time:'5 months ago',
+        views: '34M views'
     },
     {
-        thumbnail: '../images/valorant.jpg',
+        thumbnail: './images/valorant.jpg',
         uploader_pic:'https://yt3.ggpht.com/ytc/AAUvwnhbOFccxewTyhiQ4PN5JeDtQvz_cvLdsApRfdX-lyc=s48-c-k-c0x00ffffff-no-rj-mo',
         title: 'Lets play valorant',
         uploader_name: 'Mayavi Gaming',
-        upload_time:'5 months ago'
+        upload_time:'5 months ago',
+        views: '65M views'
     },
     {
-        thumbnail: '../images/payal.jpg',
+        thumbnail: './images/payal.jpg',
         uploader_pic:'https://yt3.ggpht.com/ytc/AAUvwnjNbDZnfr5v0np8rzbnr9lfrWR4npxs0Vbo8nbZLw=s176-c-k-c0x00ffffff-no-rj-mo',
         title: 'S19 RANK PUSH PUBGM with Payal gaming live',
         uploader_name: 'Payal Gaming',
-        upload_time:'2 days ago'
+        upload_time:'2 days ago',
+        views: '7M views'
     },
     {
-        thumbnail: '../images/IMG_20190217_155925.jpg',
+        thumbnail: './images/IMG_20190217_155925.jpg',
         uploader_pic:'https://yt3.ggpht.com/ytc/AAUvwniDnzVHMdlrtJn-zSHlipoxM-TcyCn2KvoKT1He=s48-c-k-c0x00ffffff-no-rj-mo',
         title: 'Kya Mujhe Pyar Hai x Tu Hi Meri Shab Hai x Labon Ko I Khudgharz',
         uploader_name: 'Khudgarz',
-        upload_time:'3 weeks ago'
+        upload_time:'3 weeks ago',
+        views: '8K views'
     }
 ]
--- youtube/src/pages/Home.js
+++ youtube/src/pages/Home.js
@@ -2,21 +2,31 @@ import React from 'react'
 import './home.css'
 import '../components/Videolist'
 import { Videolist } from '../components/Videolist';
+import {Avatar} from '@material-ui/core';
+
+
 function Home({isVisible}) {
     console.log(isVisible);
     return (
         <div className={isVisible?"sidevisible maintag":"maintag"}>
             <h1>Home</h1>
             <div className="videolist">
-                <h2>Here will be the videos.</h2>
                 {Videolist.map((items,index)=>{
                     return(
                     <div className="video_card">
-                        <img src={items.thumbnail} className="video_thumbnail"/>
-                        
-                        <p className="video_title">{items.title}</p>
-                        <p className="video_uploader">{items.uploader_name}</p>
-                        <p classname="video_time">{items.upload_time}</p>
+                        <img src={items.thumbnail} alt={items.title} className="video_thumbnail"/>
+                        <div className="video_info">
+                            <Avatar
+                                className="postAvatar"
+                                alt='icon'
+                                src={items.uploader_pic}>
+                            </Avatar>
+                            <div className="video_text">
+                                <h5 className="video_title">{items.title}</h5>
+                                <p className="video_uploader">{items.uploader_name}</p>
+                                <p classname="video_views_time">{items.views}/./{items.upload_time}</p>
+                            </div>
+                        </div>
                     </div> 
                     )   
                 })}
